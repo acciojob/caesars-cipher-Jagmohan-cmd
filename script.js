d153e3e8863750fdb2b764daf8087959423b5df8
Jagmohan-cmd <141887538+Jagmohan-cmd@users.noreply.github.com>
2023-10-13 08:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-	for (let index = 0; index < decodedArr.length; index++) {
+	for (let index = 0; index < encodedStr.length; index++) {
 			decodedArr += lookup[encodedStr[i]];
 	}
 
